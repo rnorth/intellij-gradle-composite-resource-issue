b869eb8ec37909d76fbb92315fe1a492ce68a2ef
Richard North <richard.north@skyscanner.net>
2017-03-17 14:53:02 +0000

    Add class to show that classes are resolved OK.

--- composite-build-test/src/main/java/Main.java
+++ composite-build-test/src/main/java/Main.java
@@ -1,11 +1,14 @@
 /**
- * When run via IntelliJ run, `null` will be printed to stderr.
+ * When run via IntelliJ run, `null` will be printed to stderr instead of the URL of the resource.
  *
  * When run through `./gradlew run`, a URL for the resource (which is in ../somedep/src/main/resources) will be printed.
+ *
+ * In both cases, the class ClassDependency is successfully printed.
  */
 public class Main {
 
     public static void main(String[] args) {
         System.err.println(Main.class.getClassLoader().getResource("someresource.txt"));
+        System.err.println(ClassDependency.class);
     }
 }
---
+++ somedep/src/main/java/ClassDependency.java
@@ -0,0 +1,5 @@
+/**
+ * TODO: Javadocs
+ */
+public class ClassDependency {
+}
